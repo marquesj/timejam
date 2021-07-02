eb442268c32f9385c9b92ea62575d9b3b5711475
Inês Margarida Caetano Correia <32440369+inesmargarida@users.noreply.github.com>
2021-07-02 21:38:45 +0100

    Different Tiles

--- Assets/Scripts/LifeCicles/DamageOnTouch.cs
+++ Assets/Scripts/LifeCicles/DamageOnTouch.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,14 +7,17 @@ public class DamageOnTouch : MonoBehaviour
 {
     public LayerMask layermask;
     public float damage = 1;
-    private void OnTriggerEnter(Collider other) {
+    private void OnTriggerEnter2D(Collider2D other) {
+        UnityEngine.Debug.Log("akjajaja");
         if( layermask == (layermask | (1 << other.gameObject.layer)))
         {
+
             other.gameObject.GetComponent<Health>().Damage(damage);
         }
     }
-    private void OnCollisionEnter(Collision other) 
+    private void OnCollisionEnter2D(Collision2D other) 
     {
+        UnityEngine.Debug.Log("auuu");
         if( layermask == (layermask | (1 << other.gameObject.layer)))
         {
             other.gameObject.GetComponent<Health>().Damage(damage);
--- Assets/Scripts/Palletes/DestructibleTiles.cs
+++ Assets/Scripts/Palletes/DestructibleTiles.cs
@@ -1,18 +1,28 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class DestructibleTiles : MonoBehaviour
 {
+    public Tilemap destructibleTilemap;
+    public LayerMask layermask;
     // Start is called before the first frame update
     void Start()
     {
-        
+        destructibleTilemap = GetComponent<Tilemap>();
     }
+    private void OnTriggerEnter2D(Collider2D other) {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if( layermask == (layermask | (1 << other.gameObject.layer))) {
+            
+            UnityEngine.Vector3 hitPosition = UnityEngine.Vector3.zero;
+            UnityEngine.Vector3 otherVelocity = other.gameObject.GetComponent<Rigidbody2D>().velocity;
+
+            hitPosition.x = other.transform.position.x + 0.1f * otherVelocity.x; // add bias to enter the grid cell where the tile is
+            hitPosition.y = other.transform.position.y + 0.1f * otherVelocity.y;
+            destructibleTilemap.SetTile(destructibleTilemap.WorldToCell(hitPosition), null);
+        }
     }
 }
