52a913ba6459b0d45f1bc6f5152937f29624c4ae
Inês Margarida Caetano Correia <32440369+inesmargarida@users.noreply.github.com>
2021-07-10 23:00:09 +0100

    fixes

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -21,6 +21,7 @@ public class MainMenu : MonoBehaviour
     }
 
     public void LoadGame() {
+        PlayerPrefs.SetInt("SavedScene", PlayerPrefs.GetInt("SavedScene"));
         SceneManager.LoadScene(PlayerPrefs.GetInt("SavedScene"));
 
         
